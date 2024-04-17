104bc70c649e044be022c960742f1a89631ae8af
bhoiwalapreet <123014725+bhoiwalapreet@users.noreply.github.com>
2024-04-17 13:16:45 -0400

    Revert "created new function and added items"

--- Item.java
+++ Item.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Item {
@@ -12,14 +10,14 @@ public class Item {
     private boolean isEquipped;
     private List<String> abilities;
 
-    public Item(String name, String description, int itemID, int itemHealth, int attackPwr, List<String> abilities) {
+    public Item(String name, String description, int itemID, int itemHealth, int attackPwr) {
         this.name = name;
         this.description = description;
         this.itemID = itemID;
         this.itemHealth = itemHealth;
         this.attackPwr = attackPwr;
         this.isEquipped = false;
-        this.abilities = new ArrayList<>();
+        this.abilities = null;
     }
 
     public String getName() {
@@ -59,23 +57,9 @@ public class Item {
         return isEquipped;
     }
 
-    public void addAbilities(String ability) {
-        if(this.abilities == null) {
-            this.abilities = new ArrayList<>();
-        }
+    public String addAbilities(String ability) {
         this.abilities.add(ability);
-    }
-    public static List<Item> initializeItems() {
-        //function for adding items
-
-        List<Item> items = new ArrayList<>();
-        //item id, item health, attack pwr numbers are placeholders for now
-        items.add(new Item("Sword", "A sharp blade suitable for combat.", 101, 100, 15, Arrays.asList("Slice", "Dice")));
-        items.add(new Item("Shield", "Protects the user from attacks.", 102, 150, 5, Arrays.asList("Block", "Reflect")));
-        items.add(new Item("Healing Potion", "Restores health when used.", 103, 1, 0, Arrays.asList("Heal")));
-        items.add(new Item("Magic Wand", "A mystical wand that emits magical energies.", 104, 80, 25, Arrays.asList("Cast Spell", "Magic Boost")));
-        items.add(new Item("Battle Armor", "Provides excellent protection during combat.", 105, 200, 10, Arrays.asList("Increase Defense", "Reflect Damage")));
-        return items;
+        return ability;
     }
 
     public List<String> getAbilities() {
